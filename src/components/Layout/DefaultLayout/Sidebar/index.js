f7048f8f39601406fcbcf52378f0aa7e9cf03e4e
Nguyễn Trường Sơn <97645406+truongson09112003@users.noreply.github.com>
2022-05-05 20:50:17 +0700

    Dựng khung DefaultLayout dự án Tiktok

--- src/components/Layout/DefaultLayout/Sidebar/index.js
+++
@@ -1,5 +0,0 @@
-function Sidebar() {
-    return <h2>Sidebar Bar</h2>;
-}
-
-export default Sidebar;
--- src/components/Layout/components/Header/index.js
+++
@@ -1,5 +0,0 @@
-function Header() {
-    return <h2>Header Bar</h2>;
-}
-
-export default Header;

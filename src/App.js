1204c02885167146e11f245d7268b6d8ab0ab9c8
Nguyễn Trường Sơn <97645406+truongson09112003@users.noreply.github.com>
2022-05-05 07:26:13 +0700

    update

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
+      <p>Quy định chi tiết và biện pháp thi hành Nghị quyết số 12/2021/UBTVQH15 ngày 30 tháng 12 năm 2021 của Ủy ban Thường vụ Quốc hội về việc cho phép thực hiện một số cơ chế, chính sách trong lĩnh vực y tế để phục vụ công tác phòng, chống dịch COVID-19</p>
     </div>
   );
 }
